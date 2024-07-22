1ce95c204e44bb86cefad5c2f42e78a3b97f2300
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-07-22 22:41:24 +0200

    Create 'operate' function to manage different operation cases

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let first_num, second_num, operator, result;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +14,21 @@ function mul(a, b) {
 
 function div(a, b) {
     return a / b;
+}
+
+function operate(num1, num2, op) {
+    switch (op) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = sub(num1, num2);
+            break;
+        case "*":
+            result = mul(num1, num2);
+            break;
+        case "/":
+            result = div(num1, num2);
+            break;
+    }
 }
